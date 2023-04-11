2ff0587c09a2213f0082209630f60e39b69b2366
daisiduke <jason-ti@web.de>
2023-04-11 15:53:31 +0200

    Section 10: LoveScore starter script

---
+++ section-10/loveScore.js
@@ -0,0 +1,9 @@
+var loveScore = Math.random() * 101;
+loveScore = Math.floor(loveScore);
+
+if (loveScore > 75) {
+    console.log(`Your love score is through the roof! ${loveScore}%`)
+} else {
+    console.log(`Your love score is ${loveScore}%`)
+
+}
