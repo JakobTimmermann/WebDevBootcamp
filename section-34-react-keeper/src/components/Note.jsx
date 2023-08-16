5ef90cfcc8704de081a258b8e3348ca726ed7c57
daisiduke <jason-ti@web.de>
2023-08-16 23:50:26 +0200

    Start with React Section

---
+++ section-34-react-keeper/src/components/Note.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Header () {
+    return <div className="note">
+        <h1> Title</h1>
+        <p> This is the Content</p>
+        </div>
+};
+
+export default Header
---
+++ section-34-react-keeper/src/index.jsx
@@ -0,0 +1,8 @@
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
