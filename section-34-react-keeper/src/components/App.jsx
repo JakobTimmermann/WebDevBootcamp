2cf1f27272422f96120f5cc6cbd2f0c4758716e1
daisiduke <jason-ti@web.de>
2023-08-17 11:11:21 +0200

    React Keeper: Add multiple notes with map

--- section-34-react-keeper/src/components/App.jsx
+++ section-34-react-keeper/src/components/App.jsx
@@ -2,13 +2,33 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes"
 
-function App () {
-    return <div> 
-        <Header /> 
-        <Note/> 
-        <Footer/> 
-        </div>
-    };
+function createNote(noteItem) {
+    return (
+        <Note
+            key={noteItem.id}
+            title={noteItem.title}
+            content={noteItem.content}
+        />
+    );
+}
+
+function App() {
+    return <div>
+        <Header />
+        {/* {notes.map(createNote)} */}
+        {notes.map(
+            (note) => (
+                <Note
+                    key={note.id}
+                    title={note.title}
+                    content={note.content}
+                />
+            )
+        )}
+        <Footer />
+    </div>
+};
 
 export default App
